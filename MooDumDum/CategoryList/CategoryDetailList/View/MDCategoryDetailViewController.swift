92a81e69e11e5e1d81308f00b51fd49b9bba4169
JeongHaeHyeon <wjdgo813@afreecatv.com>
2018-03-13 11:10:28 +0900

    정해현 [MooDumDum] -  DetailViewController 소스 정리

--- MooDumDum/CategoryList/CategoryDetailList/View/MDCategoryDetailViewController.swift
+++ MooDumDum/CategoryList/CategoryDetailList/View/MDCategoryDetailViewController.swift
@@ -19,7 +19,6 @@ class MDCategoryDetailViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
